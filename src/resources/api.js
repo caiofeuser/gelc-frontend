20b922ac5b6775c13412f925be71ba8ccdd6f2ea
caiofeuser <caiofeuserdm@gmail.com>
2024-05-01 17:50:31 -0300

    fix cors

--- src/resources/api.js
+++ src/resources/api.js
@@ -3,6 +3,9 @@ import { getToken } from "./auth";
 
 const api = axios.create({
   baseURL: process.env.API_BASE_URL || "https://gelc-backend.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 // const api = axios.create({ baseURL: "http://localhost:3333" });
 
